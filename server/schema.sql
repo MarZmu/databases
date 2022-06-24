e6621e7307adcea58c8b55635856de4253e445ef
Andy Chow <andychow94@gmail.com>
2022-06-24 11:06:29 -0700

    Complete designing multi-table schema

--- server/schema.sql
+++ server/schema.sql
@@ -1,15 +1,24 @@
-CREATE DATABASE chat;
+CREATE DATABASE Spotify;
 
-USE chat;
+USE Spotify;
 
-CREATE TABLE messages (
+CREATE TABLE artists (
   /* Describe your table here.*/
+  id INTEGER(10) AUTO_INCREMENT,
+  artist_name VARCHAR(25),
+  genre VARCHAR(25),
+  PRIMARY KEY (id)
 );
 
 /* Create other tables and define schemas for them here! */
-
-
-
+CREATE TABLE Playlist (
+  id INTEGER(10) AUTO_INCREMENT,
+  artist_id INTEGER(10),
+  title VARCHAR(25),
+  release_year INTEGER(4),
+  PRIMARY KEY (id),
+  FOREIGN KEY (artist_id) REFERENCES artists(id)
+);
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
